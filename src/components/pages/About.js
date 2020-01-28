eb34f35659aefba358504d4f4918d1facb6dd845
Mohamed M <mohamed613@gmail.com>
2020-01-28 11:19:52 -0500

    refactoring pf pages

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ class About extends Component {
         <section className="about-page">
           <article className="about-info">
             <p className="about-title">Mohamed M.</p>
-            <p className="about-description">Hey there. I'm a Front End Web Developer and a UI/UX Designer. I have been coding since I was 11 years old. To be more precise, I have been building applications and websites since 1999.</p>
+            <p className="about-description">Hello, there. I'm a Front-end/UI Web Developer. I have been coding since I was 11 years old. To be more precise, I have been building applications and websites since 1999.</p>
             <p className="about-description">These days I like to delve into the development side of web applications. I enjoy coding in the front end and back end. I believe in convention over configuration, and utilizing a modular methodology on the front end architecture.</p>
           </article>
           <section className="about-pic">
@@ -21,7 +21,7 @@ class About extends Component {
           <p className="abilities-title">Skillset</p>
           <ul className="abilities">
             <li className="ability-html">HTML5</li>
-            <li className="ability-css">CSS3/POSTCSS/SCSS</li>
+            <li className="ability-css">CSS3/SASS/SCSS</li>
             <li className="ability-javascript">JavaScript/ES6+/TypeScript</li>
             <li className="ability-react">React/Redux</li>
             <li className="ability-angular">Angular 2/7+</li>
--- src/components/pages/portfolio-pages/BigJump.js
+++ src/components/pages/portfolio-pages/BigJump.js
@@ -6,20 +6,21 @@ class BigJump extends Component {
     return (
       <React.Fragment>
         <main className="pf-page">
-          <h2>Big Jump Entertainment</h2>
-          <img src={require('./../../../assets/image/bigjumplogo.jpg')} alt="Big Jump Entertainment" className="pf-img" />
-      
-          <a className="pf-btn" target="_blank" href="https://bigjumpent.com/">Visit Site</a>
-      
-          <p className="pf-copy">BIG JUMP Entertainment is a privately owned, independent, 2D hybrid animation production facility that specializes in the seamless combination of traditional and digital techniques.</p>
-          
-          <img src={require('./../../../assets/image/bigjump.jpg')} alt="" className="pf-feature-img" />
-      
-          <p className="pf-copy">Lead Front End Developer on this project while working at Baytek. My team and I designed, developed and launched the new online rebranding for Big Jump Ent, a local animation studio. The deliverables I provided were the mobile, tablet and desktop front end development.</p>
-          
-          <img src={require('./../../../assets/image/bigjumpstudio.jpg')} alt="Big Jump Entertainment" className="pf-img" />
-              
-          <p className="pf-copy">The technologies were: WordPress, Divi, HTML5, CSS3, jQuery, JavaScript, Gulp, VS Code.</p>
+          <div className="pf-info">
+            <h2>Big Jump Entertainment</h2>
+            <img src={require('./../../../assets/image/bigjumplogo.jpg')} alt="Big Jump Entertainment" className="pf-logo" />
+            <a className="pf-btn" target="_blank" href="https://bigjumpent.com/">Visit Site</a>
+            </div>
+          <div className="left-panel">
+            <p className="pf-copy">BIG JUMP Entertainment is a privately owned, independent, 2D hybrid animation production facility that specializes in the seamless combination of traditional and digital techniques.</p>
+            <p className="pf-copy">I was lead front-end developer on this project while working at Baytek. My team and I designed, developed and launched the new online rebranding for Big Jump Ent, a local animation studio. The deliverables I provided were the mobile, tablet and desktop front end development.</p>
+            <p className="pf-copy">The technologies were: WordPress, Divi, HTML5, CSS3, jQuery, JavaScript, Gulp, VS Code.</p>
+          </div>
+
+          <div className="right-panel">
+            <img src={require('./../../../assets/image/bigjump.jpg')} alt="" className="pf-feature-img" />
+            <img src={require('./../../../assets/image/bigjumpstudio.jpg')} alt="Big Jump Entertainment" className="pf-img" />        
+          </div>
         </main>
     </React.Fragment>
     );
