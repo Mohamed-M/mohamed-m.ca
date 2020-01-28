3a9dc5b66457708a79420bc1ed9739ac5ee95ea1
Mohamed M <mohamed613@gmail.com>
2020-01-28 06:13:17 -0500

    Index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Header from './components/layout/Header'
 import './App.scss';
+import BackgroundVideo from './components/background-video/BackgroundVideo';
 
 function App() {
   return (
-    <Header />
+    <React.Fragment>
+      <Header />
+      <BackgroundVideo />
+      <section className="intro-page">
+        <article className="intro-info">
+          <p className="intro-title">Hey, I'm Mohamed, a Front End Web Developer.</p>
+          <p className="intro-subtitle">I'm enthusiastic about web development, great design aesthetics, and learning new technologies.</p>
+        </article>
+      </section>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/background-video/BackgroundVideo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './BackgroundVideo.scss'
+
+class BackgroundVideo extends Component {
+  render() {
+    return (
+      <section className="bg-video">
+        <div className="bg-video-wrap">
+          <div className="video-overlay"></div>
+          <video className="video" src={require('./../../assets/video/code.mp4')} autoPlay loop muted preload="auto" />
+        </div>
+      </section>
+    )
+  }
+}
+
+export default BackgroundVideo;
