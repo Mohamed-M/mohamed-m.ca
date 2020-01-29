45dbb62d502fa45fe3cf0cb08416362b7ee419c9
Mohamed M <mohamed613@gmail.com>
2020-01-28 20:49:23 -0500

    favicon

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ class About extends Component {
         <section className="about-page">
           <article className="about-info">
             <p className="about-title">About</p>
-            <p className="about-description">Hello, there. My name is Mohamed, and I'm a front-end/UI Web Developer. I'm dynamic web developer with a passion for user experience, responsive web design and enthusiastic about great code structure, efficient workflows and learning new technologies.</p>
+            <p className="about-description">Hello, there. My name is Mohamed, and I'm a front-end/UI Web Developer. I'm a dynamic web developer with a passion for user experience, responsive web design and enthusiastic about great code structure, efficient workflows and learning new technologies.</p>
             <p className="about-description">My interests and hobbies are sports, reading and watching documentaries.</p>
           </article>
           <section className="about-pic">
